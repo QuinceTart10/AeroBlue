0223a2bad123d4c586d0a2dbcbfc702c4ffc680d
QuinceTart10 <me@quincetart10.is-a.dev>
2024-06-14 05:24:16 +0200

    Add icon theme installation

---
+++ config/scripts/aero.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Clone the repo
+git clone https://gitgud.io/wackyideas/aerothemeplasma-kde6.git /tmp/atp
+
+# Extract the icon themes
+tar -xzf /tmp/atp/Icons_and_cursors/icon_themes.tar.gz -C /usr/share/icons windowsicon/
+tar -xzf /tmp/atp/Icons_and_cursors/aero-drop.tar.gz -C /usr/share/icons
