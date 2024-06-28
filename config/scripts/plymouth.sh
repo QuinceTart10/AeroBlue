e13053da6ddcef2d656f18002d210eecb71fa4b8
QuinceTart10 <me@quincetart10.is-a.dev>
2024-06-28 14:31:50 +0200

    i lost the ability to describe git commits

--- config/scripts/plymouth.sh
+++ config/scripts/plymouth.sh
@@ -13,11 +13,11 @@ export SMOD_WAIT_BOOT=6
 export SMOD_WAIT_SHUTDOWN=8
 
 # Replace placeholders in files
-sed -i "s/@Plymouth_THEMESDIR@/$PLYMOUTH_THEME_INSTALL_DIR/g" /tmp/atp/Plymouth/plymouth-theme-smod/smod.plymouth
-sed -i "s/@BOOT_ANIM_START_DELAY@/$BOOT_ANIM_START_DELAY/g" /tmp/atp/Plymouth/plymouth-theme-smod/smod.script
-sed -i "s/@BOOT_PROGRESS_CLEAR@/$BOOT_PROGRESS_CLEAR/g" /tmp/atp/Plymouth/plymouth-theme-smod/smod.script
-sed -i "s/@SMOD_WAIT_BOOT@/$SMOD_WAIT_BOOT/g" /tmp/atp/Plymouth/plymouth-theme-smod/smod-plymouth-wait-for-animation.service
-sed -i "s/@SMOD_WAIT_SHUTDOWN@/$SMOD_WAIT_SHUTDOWN/g" /tmp/atp/Plymouth/plymouth-theme-smod/smod-plymouth-wait-for-animation-poweroff.service
+sed -i "s|@Plymouth_THEMESDIR@|$PLYMOUTH_THEME_INSTALL_DIR|g" /tmp/atp/Plymouth/plymouth-theme-smod/smod.plymouth
+sed -i "s|@BOOT_ANIM_START_DELAY@|$BOOT_ANIM_START_DELAY|g" /tmp/atp/Plymouth/plymouth-theme-smod/smod.script
+sed -i "s|@BOOT_PROGRESS_CLEAR@|$BOOT_PROGRESS_CLEAR|g" /tmp/atp/Plymouth/plymouth-theme-smod/smod.script
+sed -i "s|@SMOD_WAIT_BOOT@|$SMOD_WAIT_BOOT|g" /tmp/atp/Plymouth/plymouth-theme-smod/smod-plymouth-wait-for-animation.service
+sed -i "s|@SMOD_WAIT_SHUTDOWN@|$SMOD_WAIT_SHUTDOWN|g" /tmp/atp/Plymouth/plymouth-theme-smod/smod-plymouth-wait-for-animation-poweroff.service
 
 # Move files to the appropriate locations
 mkdir /usr/share/plymouth/themes/smod
