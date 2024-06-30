575105e7d40ded0210a9d9b1fa10e738107d61b1
QuinceTart10 <me@quincetart10.is-a.dev>
2024-06-30 06:02:04 +0200

    install cmake

--- config/scripts/smod.sh
+++ config/scripts/smod.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+rpm-ostree install cmake
+
 cd /tmp/atp/KWin/cpp_effects/kwin-effect-smodsnap-v2
 cmake -B build-kf6 -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DBUILD_KF6=ON .
 make -C build-kf6
@@ -9,3 +11,5 @@ make -C build-kf6 install
 
 # i hope this works
 mv /tmp/atp/KWin/smod/snapeffecttextures.smod.rcc /usr/share/smod/snapeffecttextures.smod.rcc
+
+rpm-ostree remove cmake
