930ab511b74ecc4ccae3bcee5328463e61aead87
QuinceTart10 <me@quincetart10.is-a.dev>
2024-06-30 06:25:46 +0200

    i think i just didnt have the correct stuff installed

--- config/scripts/smod.sh
+++ config/scripts/smod.sh
@@ -2,14 +2,14 @@
 
 set -euo pipefail
 
-rpm-ostree install cmake gcc
+rpm-ostree install cmake gcc gcc-c++
 
 cd /tmp/atp/KWin/cpp_effects/kwin-effect-smodsnap-v2
-cmake -B build-kf6 -DCMAKE_CXX_COMPILER=/usr/bin/gcc -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DBUILD_KF6=ON .
+cmake -B build-kf6 -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DBUILD_KF6=ON .
 make -C build-kf6
 make -C build-kf6 install
 
 # i hope this works
 mv /tmp/atp/KWin/smod/snapeffecttextures.smod.rcc /usr/share/smod/snapeffecttextures.smod.rcc
 
-rpm-ostree remove cmake gcc
+rpm-ostree remove cmake gcc gcc-c++
