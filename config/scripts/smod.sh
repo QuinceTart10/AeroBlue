35985809d999fb3489cca4612a8a6a142fc3687d
QuinceTart10 <me@quincetart10.is-a.dev>
2024-06-30 06:35:56 +0200

    i forgor the devel package

--- config/scripts/smod.sh
+++ config/scripts/smod.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-rpm-ostree install cmake gcc gcc-c++ gmp-ecm
+rpm-ostree install cmake gcc gcc-c++ gmp-ecm gmp-ecm-devel
 
 cd /tmp/atp/KWin/cpp_effects/kwin-effect-smodsnap-v2
 cmake -B build-kf6 -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DBUILD_KF6=ON .
@@ -12,4 +12,4 @@ make -C build-kf6 install
 # i hope this works
 mv /tmp/atp/KWin/smod/snapeffecttextures.smod.rcc /usr/share/smod/snapeffecttextures.smod.rcc
 
-rpm-ostree remove cmake gcc gcc-c++ gmp-ecm
+rpm-ostree remove cmake gcc gcc-c++ gmp-ecm-devel
