cd73865b0795bfea0bc0f48e823f079ad834e008
QuinceTart10 <me@quincetart10.is-a.dev>
2024-07-11 07:47:56 +0200

    Install most of the dependencies I think

--- config/scripts/forceblur.sh
+++ config/scripts/forceblur.sh
@@ -1,4 +1,4 @@
-dnf install git cmake make
+dnf install git cmake make gcc gcc-c++ extra-cmake-modules qt6-qtbase-devel kf6-kcmutils-devel kf6-kcrash-devel kf6-ki18n-devel kf6-kio-devel kf6-kwindowsystem-devel kf6-kguiaddons-devel kf6-knotifications-devel libepoxy-devel kwin-devel wayland-devel kdecoration-devel xcb-util-devel
 git clone https://gitgud.io/wackyideas/kde-effects-forceblur.git /tmp/forceblur
 mkdir /tmp/forceblur/build
 cd /tmp/forceblur/build
