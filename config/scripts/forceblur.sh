fa7f4137ac29e2da6fe884ee86001f0795209cf8
QuinceTart10 <me@quincetart10.is-a.dev>
2024-08-18 07:29:34 +0200

    Revert "i hope this ugly mess actually works so i can pr this"
    This reverts commit a123b2876210520891697f579954b22fb491bdfc.

--- config/scripts/forceblur.sh
+++ config/scripts/forceblur.sh
@@ -9,6 +9,3 @@ cd /tmp/forceblur/build
 cmake ../ -DCMAKE_INSTALL_PREFIX=/usr
 make
 make install
-
-# Remove devel packages (I hope)
-rpm-ostree uninstall gc gcc gcc-c++ make cmake cmake-data extra-cmake-modules clang-libs clang-resource-filesystem compiler-rt guile30 qt6-rpm-macros kf6-rpm-macros qt6-linguist qt6-designer qt6-doctools qt6-qtlanguageserver $(rpm -qa | grep devel) $(rpm -qa | grep headers)
