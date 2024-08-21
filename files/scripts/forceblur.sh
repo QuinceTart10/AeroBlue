e1156845b758040babddab9c20bf488d4d8a2f4e
QuinceTart10 <me@quincetart10.is-a.dev>
2024-08-21 14:22:13 +0200

    forceblur

--- files/scripts/forceblur.sh
+++ files/scripts/forceblur.sh
@@ -3,9 +3,8 @@
 set -euo pipefail
 
 rpm-ostree install git cmake make gcc gcc-c++ extra-cmake-modules qt6-qtbase-devel kf6-kcmutils-devel kf6-kcrash-devel kf6-ki18n-devel kf6-kio-devel kf6-kwindowsystem-devel kf6-kguiaddons-devel kf6-knotifications-devel libepoxy-devel kwin-devel wayland-devel kdecoration-devel xcb-util-devel
-git clone https://gitgud.io/wackyideas/kde-effects-forceblur.git /tmp/forceblur
-mkdir /tmp/forceblur/build
-cd /tmp/forceblur/build
+mkdir /tmp/atp/kwin/effects_cpp/kde-effects-forceblur/build
+cd /tmp/atp/kwin/effects_cpp/kde-effects-forceblur/build
 cmake ../ -DCMAKE_INSTALL_PREFIX=/usr
 make
 make install
