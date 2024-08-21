88a2254ccf87f5de9e4d8e4e0ba1b5782e3cdf92
QuinceTart10 <me@quincetart10.is-a.dev>
2024-08-21 14:19:16 +0200

    Plymouth stuffs

--- files/scripts/plymouth.sh
+++ files/scripts/plymouth.sh
@@ -2,9 +2,6 @@
 
 set -euo pipefail
 
-# Extract the theme
-unzip /tmp/atp/Plymouth/plymouth-theme-smod.zip -d /tmp/atp/Plymouth
-
 # Set envvars
 export PLYMOUTH_THEME_INSTALL_DIR=/usr/share/plymouth/themes
 export BOOT_ANIM_START_DELAY=2
@@ -13,19 +10,19 @@ export SMOD_WAIT_BOOT=6
 export SMOD_WAIT_SHUTDOWN=8
 
 # Replace placeholders in files
-sed -i "s|@Plymouth_THEMESDIR@|$PLYMOUTH_THEME_INSTALL_DIR|g" /tmp/atp/Plymouth/plymouth-theme-smod/smod.plymouth
-sed -i "s|@BOOT_ANIM_START_DELAY@|$BOOT_ANIM_START_DELAY|g" /tmp/atp/Plymouth/plymouth-theme-smod/smod.script
-sed -i "s|@BOOT_PROGRESS_CLEAR@|$BOOT_PROGRESS_CLEAR|g" /tmp/atp/Plymouth/plymouth-theme-smod/smod.script
-sed -i "s|@SMOD_WAIT_BOOT@|$SMOD_WAIT_BOOT|g" /tmp/atp/Plymouth/plymouth-theme-smod/smod-plymouth-wait-for-animation.service
-sed -i "s|@SMOD_WAIT_SHUTDOWN@|$SMOD_WAIT_SHUTDOWN|g" /tmp/atp/Plymouth/plymouth-theme-smod/smod-plymouth-wait-for-animation-poweroff.service
+sed -i "s|@Plymouth_THEMESDIR@|$PLYMOUTH_THEME_INSTALL_DIR|g" /tmp/atp/misc/plymouth/plymouth-theme-smod/smod.plymouth
+sed -i "s|@BOOT_ANIM_START_DELAY@|$BOOT_ANIM_START_DELAY|g" /tmp/atp/misc/plymouth/plymouth-theme-smod/smod.script
+sed -i "s|@BOOT_PROGRESS_CLEAR@|$BOOT_PROGRESS_CLEAR|g" /tmp/atp/misc/plymouth/plymouth-theme-smod/smod.script
+sed -i "s|@SMOD_WAIT_BOOT@|$SMOD_WAIT_BOOT|g" /tmp/atp/misc/plymouth/plymouth-theme-smod/smod-plymouth-wait-for-animation.service
+sed -i "s|@SMOD_WAIT_SHUTDOWN@|$SMOD_WAIT_SHUTDOWN|g" /tmp/atp/misc/plymouth/plymouth-theme-smod/smod-plymouth-wait-for-animation-poweroff.service
 
 # Move files to the appropriate locations
 mkdir /usr/share/plymouth/themes/smod
-mv /tmp/atp/Plymouth/plymouth-theme-smod/smod.plymouth /usr/share/plymouth/themes/smod/smod.plymouth
-mv /tmp/atp/Plymouth/plymouth-theme-smod/smod.script /usr/share/plymouth/themes/smod/smod.script
-mv /tmp/atp/Plymouth/plymouth-theme-smod/assets /usr/share/plymouth/themes/smod/assets
-mv /tmp/atp/Plymouth/plymouth-theme-smod/smod-plymouth-wait-for-animation.service /usr/lib/systemd/system/smod-plymouth-wait-for-animation.service
-mv /tmp/atp/Plymouth/plymouth-theme-smod/smod-plymouth-wait-for-animation-poweroff.service /usr/lib/systemd/system/smod-plymouth-wait-for-animation-poweroff.service
+mv /tmp/atp/misc/plymouth/plymouth-theme-smod/smod.plymouth /usr/share/plymouth/themes/smod/smod.plymouth
+mv /tmp/atp/misc/plymouth/plymouth-theme-smod/smod.script /usr/share/plymouth/themes/smod/smod.script
+mv /tmp/atp/misc/plymouth/plymouth-theme-smod/assets /usr/share/plymouth/themes/smod/assets
+mv /tmp/atp/misc/plymouth/plymouth-theme-smod/smod-plymouth-wait-for-animation.service /usr/lib/systemd/system/smod-plymouth-wait-for-animation.service
+mv /tmp/atp/misc/plymouth/plymouth-theme-smod/smod-plymouth-wait-for-animation-poweroff.service /usr/lib/systemd/system/smod-plymouth-wait-for-animation-poweroff.service
 
 # Copy the Windows 7 Professional branding to the SDDM theme for consistency
 cp /usr/share/plymouth/themes/smod/assets/branding-white.png /usr/share/sddm/themes/sddm-theme-mod/Assets/branding-white.png
