598661b2bd192d275d7dc074a32d939c96652f22
QuinceTart10 <me@quincetart10.is-a.dev>
2024-08-22 18:13:26 +0200

    Hopefully build SMOD Snap 🙏

--- files/scripts/smod.sh
+++ files/scripts/smod.sh
@@ -3,8 +3,14 @@
 set -euo pipefail
 
 rpm-ostree install git cmake make gcc gcc-c++ extra-cmake-modules qt6-qtbase-devel kf6-kcmutils-devel kf6-kcrash-devel kf6-ki18n-devel kf6-kio-devel kf6-kwindowsystem-devel kf6-kguiaddons-devel kf6-knotifications-devel libepoxy-devel kwin-devel wayland-devel kdecoration-devel xcb-util-devel
+
 mkdir /tmp/atp/kwin/effects_cpp/kde-effects-forceblur/build
 cd /tmp/atp/kwin/effects_cpp/kde-effects-forceblur/build
 cmake ../ -DCMAKE_INSTALL_PREFIX=/usr
 make
 make install
+
+cd /tmp/atp/kwin/effects_cpp/kwin-effect-smodsnap-v2
+cmake -B build-kf6 -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DBUILD_KF6=ON .
+make -C build-kf6
+make -C build-kf6 install
