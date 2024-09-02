36f117ae27dd3f273d09f6c4a6ee7dc66070a6e0
QuinceTart10 <me@quincetart10.is-a.dev>
2024-09-02 18:26:59 +0200

    adapt to wacky's breaking changes

--- files/scripts/smod.sh
+++ files/scripts/smod.sh
@@ -2,10 +2,10 @@
 
 set -euo pipefail
 
-rpm-ostree install git cmake make gcc gcc-c++ extra-cmake-modules qt6-qtbase-devel kf6-kcmutils-devel kf6-kcrash-devel kf6-ki18n-devel kf6-kio-devel kf6-kwindowsystem-devel kf6-kguiaddons-devel kf6-knotifications-devel libepoxy-devel kwin-devel wayland-devel kdecoration-devel xcb-util-devel kf6-kiconthemes-devel kf6-kirigami-devel kf6-frameworkintegration-devel
+rpm-ostree install git cmake make gcc gcc-c++ extra-cmake-modules qt6-qtbase-devel kf6-kcmutils-devel kf6-kcrash-devel kf6-ki18n-devel kf6-kio-devel kf6-kwindowsystem-devel kf6-kguiaddons-devel kf6-knotifications-devel libepoxy-devel kwin-devel wayland-devel kdecoration-devel xcb-util-devel kf6-kiconthemes-devel kf6-kirigami-devel kf6-frameworkintegration-devel kf6-ksvg-devel
 
-mkdir /tmp/atp/kwin/effects_cpp/kde-effects-forceblur/build
-cd /tmp/atp/kwin/effects_cpp/kde-effects-forceblur/build
+mkdir /tmp/atp/kwin/effects_cpp/kde-effects-aeroglassblur/build
+cd /tmp/atp/kwin/effects_cpp/kde-effects-aeroglassblur/build
 cmake ../ -DCMAKE_INSTALL_PREFIX=/usr
 make
 make install
